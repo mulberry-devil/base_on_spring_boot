1e8e659eb6059eff2e6f10f969b169bfb7c62b41
caston_chen <caston_chen@Caston-Chen-NB.macroviewhk.com>
2022-03-26 10:54:07 +0800

    elasticsearch -v1

---
+++ src/main/java/com/caston/base_on_spring_boot/elasticsearch/config/ElasticsearchConfig.java
@@ -0,0 +1,18 @@
+package com.caston.base_on_spring_boot.elasticsearch.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+    @Value("${spring.elasticsearch.rest.uris}")
+    private String uris;
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration configuration = ClientConfiguration.builder().connectedTo(uris).build();
+        return RestClients.create(configuration).rest();
+    }
+}
