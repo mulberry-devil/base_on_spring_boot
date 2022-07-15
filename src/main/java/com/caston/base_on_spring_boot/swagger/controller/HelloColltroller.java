99a2a752f4e734509d0185f1b0b4152637540aca
caston_chen <caston_chen@Caston-Chen-NB.macroviewhk.com>
2022-07-15 17:22:31 +0800

    update

--- src/main/java/com/caston/base_on_spring_boot/swagger/controller/HelloColltroller.java
+++ src/main/java/com/caston/base_on_spring_boot/swagger/controller/HelloColltroller.java
@@ -1,5 +1,10 @@
 package com.caston.base_on_spring_boot.swagger.controller;
 
+import com.aliyun.ocr_api20210707.Client;
+import com.aliyun.ocr_api20210707.models.RecognizeGeneralRequest;
+import com.aliyun.ocr_api20210707.models.RecognizeGeneralResponse;
+import com.aliyun.teaopenapi.models.Config;
+import com.aliyun.teautil.models.RuntimeOptions;
 import com.caston.base_on_spring_boot.swagger.entity.Hello;
 import io.swagger.annotations.*;
 import org.springframework.web.bind.annotation.*;
@@ -43,7 +48,28 @@ public class HelloColltroller {
 
     @ApiOperation("测试文件上传信息")
     @PostMapping("/importFile")
-    public String importFile(@RequestPart MultipartFile file) {
-        return file.getOriginalFilename();
+    public String importFile(@RequestPart MultipartFile file) throws Exception {
+        Config authConfig = new Config();
+        authConfig.accessKeyId = "LTAI4G2oMkHaoqRXWWXL6r3j";
+        authConfig.accessKeySecret = "Ua4ezurQICbrPPfrR8YlxKtHWKGkMD";
+        // 文字识别的服务入口
+        authConfig.endpoint = "ocr-api.cn-hangzhou.aliyuncs.com";
+
+        Client client = new Client(authConfig);
+
+        // 2 创建RuntimeObject实例并设置运行参数
+        RuntimeOptions runtime = new RuntimeOptions();
+        // 是否开启失败重试
+        runtime.autoretry = true;
+        // 最大重试次数
+        runtime.maxAttempts = 5;
+
+        // 3 创建API请求并设置参数
+        RecognizeGeneralRequest request = new RecognizeGeneralRequest();
+        request.setBody(file.getInputStream());
+        // 4 发起请求并处理应答或异常
+        RecognizeGeneralResponse response = client.recognizeGeneralWithOptions(request, runtime);
+        System.out.println(response.getBody().getData());
+        return response.getBody().getData();
     }
 }
